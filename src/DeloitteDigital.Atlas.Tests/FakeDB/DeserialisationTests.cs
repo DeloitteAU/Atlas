c660f2e915c03a87cd19b60c56b1237cd307b5db
ddbne <ddbne@deloitte.com.au>
2020-04-07 13:00:51 +1000

    Switch to PackageReference for Nuget Update nuspec to requier Sitecore MVC 9.3 and .NET 4.7.2

--- src/DeloitteDigital.Atlas.Tests/FakeDB/DeserialisationTests.cs
+++ src/DeloitteDigital.Atlas.Tests/FakeDB/DeserialisationTests.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Shouldly;
+using Xunit;
 
 namespace DeloitteDigital.Atlas.Tests.FakeDB
 {
-    [TestClass]
     public class DeserialisationTests
     {
-        [TestMethod]
+        [Fact]
         public void DeserialisationShouldReturnSitecoreItem()
         {
             using (var db = new DspFakeDb())
             {
                 var home = db.GetHomeItem();
 
-                // ensure the de-serialisation worked 
-                Assert.IsNotNull(home);
-                Assert.AreEqual("Home", home.Name);
-                Assert.AreEqual("Sitecore Experience Platform", home.Fields["Title"].ToString());
+                // ensure the de-serialisation worked
+                home.ShouldNotBeNull();
+                home.Name.ShouldBe("Home");
+                home.Fields["Title"].ToString().ShouldBe("Sitecore Experience Platform");
             }
         }
 
